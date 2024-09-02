0e454fa80fcdc1db1629d97c3d47aee214d6a2f8
Gabriel Matiolli <gabrielmatiollif@gmail.com>
2024-09-02 11:00:25 -0300

    Criação da estrutura de navegação

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-function App() {
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
+function App() {
   return (
     <>
-      
+      <Nav />
+      <h1>Main</h1>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Erro from "./components/Error.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Erro />,
+    children: [
+      {
+        path: "/algo",
+        element: "<Algo />",
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
