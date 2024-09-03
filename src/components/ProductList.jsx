679c9643fc815074ca7e09f58a604fcdbc473eac
Berla1 <gustavoberlanga312@gmail.com>
2024-09-03 09:12:14 -0300

    dividindo o produtos.json em 3

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,31 @@
 import ProductCard from "./ProductCard";
 import { ContainerProducts } from "../styles/ProductStyle";
-import products from '../mocks/produtos.json';
+import emAlta from "../mocks/alta.json";
+import materialEsportivo from "../mocks/material-esportivo.json";
+import ofertas from "../mocks/ofertas.json";
 
 const ProductList = () => {
   return (
     <ContainerProducts>
-      {products.map((product) => (
+      {emAlta.map((product) => (
+        <ProductCard
+          key={product.id}
+          produto={product.produto}
+          preco={product.preco}
+          src={product.src}
+        />
+      ))}
+
+      {materialEsportivo.map((product) => (
+        <ProductCard
+          key={product.id}
+          produto={product.produto}
+          preco={product.preco}
+          src={product.src}
+        />
+      ))}
+
+      {ofertas.map((product) => (
         <ProductCard
           key={product.id}
           produto={product.produto}
