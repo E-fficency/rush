9c53aab5d1f5513d290c55201b8b0d613d27cc02
camfeitosa <camilafeitosacoutinho@gmail.com>
2024-09-04 19:25:42 -0300

    Ajustes no pruduto

--- src/styles/ProductDetails.jsx
+++ src/styles/ProductDetails.jsx
@@ -3,27 +3,34 @@ import { IoIosArrowDown } from "react-icons/io";
 
 
 export const MainProductDetails = styled.div`
-  padding: 0 1.4rem;
+  /* padding: 0 1.4rem; */
+  @media (min-width: 600px) {
+    padding: 0 3rem;
+  }
 `;
 
 export const ContainerProductDetails = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  gap: 0.7rem;
   align-items: center;
   @media (min-width: 600px) {
-    width: 40%;
+    flex-direction: row;
+    gap: 2rem;
   }
 `;
 
 export const StyledImg = styled.div`
-  height: 500px;
+  height: 60vh;
   width: 100%;
   img {
     object-fit: cover;
     width: 100%;
     height: 100%;
+    padding: 0 1.2rem;
+  }
+  @media (min-width: 600px) {
+    height: 80vh;
   }
 `;
 
@@ -32,22 +39,24 @@ export const ProductDetail = styled.div`
   flex-direction: column;
   gap: 0.8rem;
   min-height: 300px;
-  /* backdrop-filter: blur(70px); */
   z-index: 1;
-
+  padding: 0 1.5rem;
+  /* backdrop-filter: blur(70px); */
   span {
     font-weight: 600;
     font-size: 1.25rem;
     color: #b02b2b;
+    margin-top: 1rem;
   }
   h2 {
-    font-size: 1.25rem;
+    font-size: 1rem;
     font-weight: 500;
   }
   p {
     font-size: 0.875rem;
     font-weight: 400;
   }
+
 `;
 
 export const AdicionarASacola = styled.button`
@@ -94,8 +103,8 @@ export const ContainerIcon = styled.div`
   height: 70px;
   border-radius: 100%;
   position: absolute;
-  right: 0;
-  bottom: 100%;
+  right: 5%;
+  bottom: 90%;
   cursor: pointer;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   svg {
@@ -107,12 +116,19 @@ export const ContainerIcon = styled.div`
   }
 `;
 
-export const Gradient = styled.div`
-  background: linear-gradient(180deg, transparent, #ffffff);
-    bottom: 100%;
-    position: absolute;
-    height: 30%;
-    left: 0;
-    width: 100%;
-    z-index: -9;
-`
+export const Gradient = styled.img`
+  /* background: linear-gradient(180deg, transparent, #ffffff); */
+  bottom: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  z-index: -9;
+  user-select: none;
+  -webkit-user-select: none; 
+  -moz-user-select: none;    
+  -ms-user-select: none;
+  @media (min-width: 600px) {
+    display: none;
+  }
+`;
