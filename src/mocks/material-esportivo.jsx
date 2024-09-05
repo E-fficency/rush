009cdb86607e2895820c73905fba5c679398dbba
Gabriel Matiolli <gabrielmatiollif@gmail.com>
2024-09-05 10:59:04 -0300

    caminho das imagens

--- src/mocks/material-esportivo.jsx
+++ src/mocks/material-esportivo.jsx
@@ -1,32 +1,41 @@
-const materialEsportivo = [
-    {
-        "id": 5,
-        "src": "/assets/images/produtos/chuteira-nike-mercurial-vapor-15.svg",
-        "produto": "Chuteira Nike Mercurial Vapor 15 Pro Society",
-        "descricao": "O campo é seu quando você calça a ousada Vapor 15 Pro TF. Com uma unidade Zoom Air para que você possa dominar nos minutos finais de uma partida - quando mais importa. A velocidade está no Air.",
-        "preco": 959.99
-    },
-    {
-        "id": 6,
-        "src": "/assets/images/produtos/chuteira-nike-superfly-9.svg",
-        "produto": "Chuteira Nike Mercurial Superfly 9 Academy Futsal",
-        "descricao": "Incline instantaneamente o campo com o design arrojado do Superfly 9 Academy IC. Com uma unidade Zoom Air e NikeSkin flexível na parte de cima para toque excepcional, para que você possa dominar nos minutos finais de uma partida - quando mais importa. A velocidade está no Air.",
-        "preco": 560.49
-    },
-    {
-        "id": 7,
-        "src": "/assets/images/produtos/chuteira-nike-zoom-mercurial-superfly.svg",
-        "produto": "Chuteira Nike Zoom Mercurial Superfly 9 Campo",
-        "descricao": "Incline instantaneamente o campo com o design arrojado do Superfly 9 Academy MG. Com uma unidade Zoom Air e NikeSkin flexível na parte de cima para toque excepcional, para que você possa dominar nos minutos finais de uma partida - quando mais importa. A velocidade está no Air.",
-        "preco": 474.99
-    },
-    {
-        "id": 8,
-        "src": "/assets/images/produtos/tenis-nike-zoom-lebron-nxxt.svg",
-        "produto": "Tênis Nike Zoom LeBron NXXT Gen Masculino",
-        "descricao": "Quer seja um saltador do tipo que faz enterradas ou aquele que está sempre vagando pela linha de base, sinta-se mais rápido, mais baixo para a quadra e seguro no LeBron NXXT Gen. Nós o adaptamos especificamente para atender às demandas do ritmo acelerado do jogo de hoje em dia, para que você possa ficar à frente da oposição com sua velocidade em todas as direções.",
-        "preco": 1119.99
-    }
-]
+import nikeMercurialVapor from "../assets/images/produtos/chuteira-nike-mercurial-vapor-15.svg";
+import nikeSuperFly from "../assets/images/produtos/chuteira-nike-superfly-9.svg";
+import nikeZoomMercurial from "../assets/images/produtos/chuteira-nike-zoom-mercurial-superfly.svg";
+import nikeZoomLebron from "../assets/images/produtos/tenis-nike-zoom-lebron-nxxt.svg";
 
-export default materialEsportivo
+const materialEsportivo = [
+  {
+    id: 5,
+    src: nikeMercurialVapor,
+    produto: "Chuteira Nike Mercurial Vapor 15 Pro Society",
+    descricao:
+      "O campo é seu quando você calça a ousada Vapor 15 Pro TF. Com uma unidade Zoom Air para que você possa dominar nos minutos finais de uma partida - quando mais importa. A velocidade está no Air.",
+    preco: 959.99,
+  },
+  {
+    id: 6,
+    src: nikeSuperFly,
+    produto: "Chuteira Nike Mercurial Superfly 9 Academy Futsal",
+    descricao:
+      "Incline instantaneamente o campo com o design arrojado do Superfly 9 Academy IC. Com uma unidade Zoom Air e NikeSkin flexível na parte de cima para toque excepcional, para que você possa dominar nos minutos finais de uma partida - quando mais importa. A velocidade está no Air.",
+    preco: 560.49,
+  },
+  {
+    id: 7,
+    src: nikeZoomMercurial,
+    produto: "Chuteira Nike Zoom Mercurial Superfly 9 Campo",
+    descricao:
+      "Incline instantaneamente o campo com o design arrojado do Superfly 9 Academy MG. Com uma unidade Zoom Air e NikeSkin flexível na parte de cima para toque excepcional, para que você possa dominar nos minutos finais de uma partida - quando mais importa. A velocidade está no Air.",
+    preco: 474.99,
+  },
+  {
+    id: 8,
+    src: nikeZoomLebron,
+    produto: "Tênis Nike Zoom LeBron NXXT Gen Masculino",
+    descricao:
+      "Quer seja um saltador do tipo que faz enterradas ou aquele que está sempre vagando pela linha de base, sinta-se mais rápido, mais baixo para a quadra e seguro no LeBron NXXT Gen. Nós o adaptamos especificamente para atender às demandas do ritmo acelerado do jogo de hoje em dia, para que você possa ficar à frente da oposição com sua velocidade em todas as direções.",
+    preco: 1119.99,
+  },
+];
+
+export default materialEsportivo;
