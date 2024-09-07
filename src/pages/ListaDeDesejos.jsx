6b16a62443cee515ed8b9ed43aa1a6bf2ccdcfa9
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-09-07 01:39:40 -0300

    Página de lista de desejos.

--- src/pages/ListaDeDesejos.jsx
+++ src/pages/ListaDeDesejos.jsx
@@ -1,20 +1,49 @@
 import { Link } from "react-router-dom";
 import { StyledNav } from "../styles/StyleNav";
-import { IoArrowBackOutline } from "react-icons/io5";
-import { useEffect } from "react";
+import { IoArrowBackOutline, IoHeartSharp } from "react-icons/io5";
+import { useEffect, useState } from "react";
+import {
+  ItemInfos,
+  ItemListaDeDesejos,
+  MainContainerListaDeDesejos,
+  TituloListaDesejos,
+} from "../styles/StyleListaDeDesejos";
 
 const ListaDeDesejos = () => {
+  const [favoritos, setFavoritos] = useState([]);
+  useEffect(() => {
+    const pegarFavoritos = localStorage.getItem("favoritos");
+    if (pegarFavoritos) {
+      setFavoritos(JSON.parse(pegarFavoritos));
+    }
+    document.title = "Rush | Lista de desejos";
+  }, []);
 
-    useEffect(() => {
-        document.title = 'Rush | Lista de desejos'
-      })
-    return (  
-        <>
-            <StyledNav>
-               <Link to={'/'}><IoArrowBackOutline fontSize={"2em"} color={"black"} /></Link> 
-            </StyledNav>
-        </>
-    );
-}
- 
+  return (
+    <>
+      <StyledNav>
+        <Link to={"/"}>
+          <IoArrowBackOutline fontSize={"2em"} color={"black"} />
+        </Link>
+      </StyledNav>
+      <MainContainerListaDeDesejos>
+        <TituloListaDesejos>Lista de desejos</TituloListaDesejos>
+        <ItemListaDeDesejos>
+          {favoritos.map((item) => (
+            <ItemInfos>
+              <div>
+                <img src={item.src} />
+              </div>
+              <div style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}> 
+                <h2>{item.nome}</h2>
+                <IoHeartSharp color="red" size={25} />
+              </div>
+            </ItemInfos>
+          ))}
+        </ItemListaDeDesejos>
+      </MainContainerListaDeDesejos>
+    </>
+  );
+};
+
 export default ListaDeDesejos;
