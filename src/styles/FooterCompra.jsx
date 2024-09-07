8123193076b3b0626d96a2cad2a7c4cc80db6a63
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-09-07 16:18:09 -0300

    footer desktop

--- src/styles/FooterCompra.jsx
+++ src/styles/FooterCompra.jsx
@@ -7,7 +7,7 @@ export const Footer = styled.footer`
   height: 18dvh;
   align-items: center;
   justify-content: space-around;
-  background-color: #F2F2F2;
+  background-color: #ffffff;
   box-shadow: 0px 1px 18.1px 0px rgba(0, 0, 0, 0.13);
   position: fixed;
   z-index: 100;
